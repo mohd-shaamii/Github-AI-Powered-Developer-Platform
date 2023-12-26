af69a5d63ea7532323593a2d66b58b8c47a26184
Vexy-04 <varunal2003@gmail.com>
2023-12-26 12:14:41 +0530

    prime

--- Topic 3/prime.py
+++ Topic 3/prime.py
@@ -6,8 +6,9 @@ def is_prime(n):
             return False
     return True
 
-number = int(input("Enter a number: "))
-if is_prime(number):
-    print(number, "is a prime number")
-else:
-    print(number, "is not a prime number")
+primes = []
+for num in range(0, 101):
+    if is_prime(num):
+        primes.append(num)
+
+print(primes)
